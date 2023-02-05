9e11be76c090bd0477732c62377d3bf0f531c5e1
Piotr Kawula <kontakt@piotrkawula.pl>
2023-02-05 21:34:55 +0100

    Pre-commit test

--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,36 +1,36 @@
-import {createRouter, createWebHistory} from 'vue-router';
-import {UrlBase} from "../utils/Utils";
-import {UserStore} from "../stores/user";
+import { createRouter, createWebHistory } from "vue-router";
+import { UrlBase } from "../utils/Utils";
+import { UserStore } from "../stores/user";
 
 const router = createRouter({
-    history: createWebHistory(UrlBase), // import.meta.env.BASE_URL ||
-    routes: [
-        {
-            path: '/',
-            name: 'login',
-            component: () => import('../views/AccessControl/Login.vue')
-        },
-        {
-            path: '/dashboard',
-            name: 'dashboard',
-            component: () => import('../views/Dashboard.vue')
-        },
-        {
-            path: '/resource/index',
-            name: 'resource-index',
-            component: () => import('../views/Resource/Index.vue'),
-        }
-    ]
+  history: createWebHistory(UrlBase), // import.meta.env.BASE_URL ||
+  routes: [
+    {
+      path: "/",
+      name: "login",
+      component: () => import("../views/AccessControl/Login.vue"),
+    },
+    {
+      path: "/dashboard",
+      name: "dashboard",
+      component: () => import("../views/Dashboard.vue"),
+    },
+    {
+      path: "/resource/index",
+      name: "resource-index",
+      component: () => import("../views/Resource/Index.vue"),
+    },
+  ],
 });
 
 router.beforeEach(async (to, from, next) => {
-    const store = UserStore();
+  const store = UserStore();
 
-    if (to.name !== 'login' && !await store.isAuthenticated(true)) {
-        next({name: 'login'});
-    } else {
-        next();
-    }
+  if (to.name !== "login" && !(await store.isAuthenticated(true))) {
+    next({ name: "login" });
+  } else {
+    next();
+  }
 });
 
-export default router
+export default router;
